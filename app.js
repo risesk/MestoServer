1afb46f4b6437aa77ee2f1484605541f8c89d150
risesk <antonkuvichka@gmail.com>
2019-10-26 00:06:01 +0300

    database interface added

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express');
-const path = require('path');
-const router = require('./routes/routes');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+const { wrongPage } = require('./routes/wrongpage');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
-app.use(router);
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5db1fd3e506a2121e0debc2a'
+  };
+  next();
+});
+
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
+app.use('*', wrongPage);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
